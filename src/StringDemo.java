2ebedc748e6231e14ac8cc72c2969b36f148106a
Tony.Yang <tonyyang1029@gmail.com>
2019-09-26 13:59:21 +0800

    Modify variable names.

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -6,27 +6,27 @@ import java.util.Date;
 
 public class StringDemo {
     public static void main(String args[]) {
-        long a = new Date().getTime();
-        String cc = "";
+        long s1 = System.currentTimeMillis();
+        String str = "";
         int n = 10000;
 
         for (int i = 0; i < n; i++) {
-            cc += "." + i;
+            str += "." + i;
         }
-        System.out.println("String: " + (System.currentTimeMillis() - a) / 1000.0 + "s");
+        System.out.println("String: " + (System.currentTimeMillis() - s1) / 1000.0 + "s");
 
-        long s1 = System.currentTimeMillis();
+        long s2 = System.currentTimeMillis();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             sb.append("." + 1);
         }
-        System.out.println("StringBuilder: " + (System.currentTimeMillis() - s1) / 1000.0 + "s");
+        System.out.println("StringBuilder: " + (System.currentTimeMillis() - s2) / 1000.0 + "s");
 
-        long s2 = System.currentTimeMillis();
+        long s3 = System.currentTimeMillis();
         StringBuffer sbf = new StringBuffer();
         for (int i = 0; i < n; i++) {
             sbf.append("." + i);
         }
-        System.out.println("StringBuffer: " + (System.currentTimeMillis() - s2) / 1000.0 + "s");
+        System.out.println("StringBuffer: " + (System.currentTimeMillis() - s3) / 1000.0 + "s");
     }
 }
